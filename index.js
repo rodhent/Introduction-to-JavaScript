d47784d9542962f4c01ff22de353bce4398e51c6
Rod Hentringer <67014030+rodhent@users.noreply.github.com>
2020-08-12 11:14:50 -0400

    javascript update

--- index.js
+++ index.js
@@ -35,10 +35,10 @@ console.log(multiply(2, 4));
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years
 
-function humanAge(age){
-    return age * 7;
-  }
-  console.log (humanAge(33));
+function humanAge(age) {
+  return age * 7;
+}
+console.log(humanAge(33));
 
 /************************************************************** Task 3 **************************************************************/
 //Dog feeder
@@ -58,27 +58,26 @@ function humanAge(age){
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
 
-function dogfeeder(weight,age){
-    if(age >= 1 && weight <= 5){
-      return weight * 0.05;
-    }else if(age >= 1 && weight >= 6 && weight <= 10){
-      return weight * 0.04;
-    }else if(age >= 1 && weight >= 11 && weight <=15){
-      return weight * 0.03;  
-    }else if(age >= 1 && weight > 15){
-      return weight * 0.02;
-    }else if(age >= .16  && age <= 0.33){
-      return weight * 0.10;
-    }else if(age >= .33  && age <= 0.58){
-      return weight * 0.10;
-    }else if(age >= 0.58  && age <= 1){
-      return weight * 0.10;
-    }else{
-      console.log("error");
-    }
+function dogfeeder(weight, age) {
+  if (age >= 1 && weight <= 5) {
+    return weight * 0.05;
+  } else if (age >= 1 && weight >= 6 && weight <= 10) {
+    return weight * 0.04;
+  } else if (age >= 1 && weight >= 11 && weight <= 15) {
+    return weight * 0.03;
+  } else if (age >= 1 && weight > 15) {
+    return weight * 0.02;
+  } else if (age >= 0.16 && age <= 0.33) {
+    return weight * 0.1;
+  } else if (age >= 0.33 && age <= 0.58) {
+    return weight * 0.1;
+  } else if (age >= 0.58 && age <= 1) {
+    return weight * 0.1;
+  } else {
+    console.log("error");
   }
-    console.log(dogfeeder(15,1));
-
+}
+console.log(dogfeeder(15, 1));
 
 /************************************************************** Task 4 **************************************************************/
 // Rock, Paper, Sissors
@@ -87,26 +86,45 @@ function dogfeeder(weight,age){
 // use math.random to determine the computers choice
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number
 
+let computerChoice = Math.random();
+if (computerChoice <= 0.5) {
+  computerChoice = "rock";
+} else if (computerChoice <= 0.5) {
+  computerChoice = "paper";
+} else {
+  computerChoice = "sissors";
+}
 
-Working...
-
-
+function game(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return "It's a tie!";
+  } else if (userChoice === "rock" && computerChoice === "sissors") {
+    return "You win";
+  } else if (userChoice === "paper" && computerChoice === "rock") {
+    return "You win";
+  } else if (userChoice === "sissors" && computerChoice == "paper") {
+    return "You win";
+  } else {
+    return "You lose";
+  }
+}
+console.log(game("rock", computerChoice));
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
 function kmtomiles(km) {
-    return km / 1.609344
+  return km / 1.609344;
 }
-console.log(kmtomiles(8))
+console.log(kmtomiles(8));
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
 
 function feettocm(ft) {
-    return ft * 30.48
+  return ft * 30.48;
 }
-console.log(feettocm(8))
+console.log(feettocm(8));
 
 /************************************************************** Task 6 **************************************************************/
 // 99 bottles of soda on the wall
@@ -122,27 +140,38 @@ console.log(feettocm(8))
 //60s should be D
 //and anything below 60 should be F
 
-function grades(grade){
-    if (grade >= 90){
-      return 'A'
-  } else if (grade >= 80 && grade < 90){
-      return 'B'
-  } else if (grade >= 70 && grade < 80){
-      return 'C'
-  } else if (grade >= 60 && grade < 70){
-      return 'D'
+function grades(grade) {
+  if (grade >= 90) {
+    return "A";
+  } else if (grade >= 80 && grade < 90) {
+    return "B";
+  } else if (grade >= 70 && grade < 80) {
+    return "C";
+  } else if (grade >= 60 && grade < 70) {
+    return "D";
   } else {
-      return 'F'
+    return "F";
   }
 }
-console.log(grades(90))
+console.log(grades(90));
 
 /************************************************************** Stretch **************************************************************/
 //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
 // Hint - you may need to study tomorrow's traning kit on arrays
 // try looking up the .includes() method
 
+function vowel_count(str1) {
+  var vowel_list = "AEIOUaeiou";
+  var vowel_count = 0;
 
+  for (var x = 0; x < str1.length; x++) {
+    if (vowel_list.indexOf(str1[x]) !== -1) {
+      vowel_count += 1;
+    }
+  }
+  return vowel_count;
+}
+console.log(vowel_count("How to count vowels"));
 
 /************************************************************** Stretch **************************************************************/
 //Take Rock, Paper, Sissors further
